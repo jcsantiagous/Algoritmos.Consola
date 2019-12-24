44beda9b51f26e07061d1b45064bc54727993cc6
Juan Carlos Santiago <jcsantiagous@gmail.com>
2019-12-24 14:09:17 -0600

    demo de piramide de un numero

--- Algoritmos.Consola/Program.cs
+++ Algoritmos.Consola/Program.cs
@@ -10,6 +10,36 @@ namespace Algoritmos.Consola
     {
         static void Main(string[] args)
         {
+            int num;
+            string linea;
+            Console.Write("INGRESA UN NUMERO: ");
+            linea = Console.ReadLine();
+
+            num = int.Parse(linea);
+
+            if (num > 0)
+            {
+                for (int r = 1; r <= num; r++)
+                {
+                    for (int c = 1; c <= r; c++)
+                    {
+                        Console.Write("{0} ", c);
+                    }
+                    Console.Write(Environment.NewLine);
+                }
+
+                for (int r = num -1 ; r > 0; r--)
+                {
+                    for (int c = 1; c <= r; c++)
+                    {
+                        Console.Write("{0} ", c);
+                    }
+                    Console.Write(Environment.NewLine);
+                }
+
+            }
+
+            Console.Read();
         }
     }
 }
